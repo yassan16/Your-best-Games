1a6e32dae01dc1216a41c4f6f1920d49e61675f6
保田壮太 <yasudasota@MacBook-Pro.local>
2020-08-16 16:29:15 +0900

    before first deploy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :authenticate_user!, only:[:new, :create, :destroy]
+	before_action :authenticate_user!, only:[:new, :create, :show, :destroy]
 	before_action :get_post, only:[:show, :destroy]
 
 	def new
